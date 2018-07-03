42e973095f6cd01afd11aa6c329ecf3c6bec0eb3
zhou <2891128968@qq.com>
2018-07-03 16:30:48 +0800

    2018.7.3 16:28 引入了解决vue组件加载html作为template的插件

--- src/main/webapp/module/topUserInfo/topUserInfo.js
+++ src/main/webapp/module/topUserInfo/topUserInfo.js
@@ -1,6 +1,24 @@
-Vue.component('demo-item',{
-    props:{
-        unit:''
-    },
-    template:'<li>{{unit.type}}</li>'
+Vue.component('demo', function (resolve, reject) {
+    GL.Http.load('./module/topUserInfo/topUserInfo.html', function (responseText) {
+        resolve({
+            template: responseText,
+            props: {
+                left: {
+                    type: Number,
+                    default: 0
+                },
+                right: {
+                    type: Number,
+                    default: 0
+                }
+            },
+            data: function () {
+                return {}
+            },
+            watch: {},
+            created: function () {},
+            methods: {}
+        });
+    });
 });
+
