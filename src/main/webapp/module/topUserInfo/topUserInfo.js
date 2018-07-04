5cca7b1176dc2213665b08271d65b175be881313
zhou <2891128968@qq.com>
2018-07-04 20:47:52 +0800

    2018.7.4 20:47 登录界面开发

--- src/main/webapp/module/topUserInfo/topUserInfo.js
+++ src/main/webapp/module/topUserInfo/topUserInfo.js
@@ -4,11 +4,36 @@ Vue.component('top-user-info', function (resolve, reject) {
             template: responseText,
             props: {},
             data: function () {
-                return {}
+                return {
+                    loginModal:false,
+                    loginFlag:false,
+                    loginInfo:{
+                        username:'',
+                        password:'',
+                        vaild:''
+                    },
+                    loginRule:''
+                }
             },
             watch: {},
             created: function () {},
-            methods: {}
+            methods: {
+                selectMyShop:function (name) {
+                    console.log(name)
+                },
+                login:function () {
+                    console.log("正在登录")
+                    this.loginFlag= true;
+                    setTimeout(() => {
+                        this.loginModal = false;
+                        this.loginFlag= false;
+                        this.$Message.success('登录成功');
+                    }, 2000);
+                },
+                showLogin:function () {
+                    this.loginModal=true;
+                }
+            }
         });
     });
 });
