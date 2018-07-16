5bf28c9724d70a60be746a433215837b4b696ce4
zhou <2891128968@qq.com>
2018-07-16 20:45:08 +0800

    2018.7.16 20:44 订单界面的开发以及订单代码的优化

---
+++ src/main/webapp/module/orderUnit/orderUnit.js
@@ -0,0 +1,18 @@
+Vue.component('order-unit', function (resolve, reject) {
+    GL.Http.load('./module/orderUnit/orderUnit.html', function (responseText) {
+        resolve({
+            template: responseText,
+            props: {
+                orderInfo:Object
+            },
+            data: function () {
+                return {
+                    productCount:1
+                }
+            },
+            watch: {},
+            created: function () {},
+            methods: {}
+        });
+    });
+});
