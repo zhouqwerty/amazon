9e18c13cc92080b2647a7481126067fe53cf0adc
zhou <2891128968@qq.com>
2018-07-22 16:37:17 +0800

    2018.7.22 16:36 配置swagger

--- src/main/java/com/amazon/module/controller/UserController.java
+++ src/main/java/com/amazon/module/controller/UserController.java
@@ -2,28 +2,33 @@ package com.amazon.module.controller;
 
 import com.amazon.module.entity.User;
 import com.amazon.module.service.UserService;
+import io.swagger.annotations.Api;
+import io.swagger.annotations.ApiOperation;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
-import org.springframework.web.servlet.ModelAndView;
-
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.ResponseBody;
 import java.util.Date;
 
-
+@Api(description = "用户接口")
 @Controller
 @RequestMapping("/user")
 public class UserController {
 
     @Autowired
     private UserService us;
-
-    @RequestMapping("/login")
-    public ModelAndView login(User user){
-        return null;
+    @ApiOperation(value = "登录")
+    @RequestMapping(value = "/login",method = RequestMethod.POST)
+    @ResponseBody
+    public String login(@RequestBody User user){
+        return "1";
     }
 
-    @RequestMapping("/register")
-    public  String register(User user){
+    @RequestMapping(value = "/register",method = RequestMethod.POST)
+    @ResponseBody
+    public  String register(@RequestBody User user){
         User u=new User();
         u.setUsername("demo");
         u.setPassword("123");
@@ -32,13 +37,14 @@ public class UserController {
         u.setEmail("123@123.com");
         u.setAddress("china");
         u.setMobile("110");
-        u.setSex("m");
+        u.setSex("1");
         us.registerUser(u);
         return "1";
     }
 
-    @RequestMapping("/checkEmail")
-    public String checkEmail(User user){
+    @RequestMapping(value = "/checkEmail",method = RequestMethod.POST)
+    @ResponseBody
+    public String checkEmail(@RequestBody User user){
         return null;
     }
 
